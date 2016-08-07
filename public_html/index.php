e7d27112ba80abcc53b049fbc617dc513f162966
Joonas Tiala <joonas.tiala@gmail.com>
2016-08-07 09:49:27 -0400

    Refined folder structure

--- public_html/index.php
+++ public_html/index.php
@@ -9,12 +9,12 @@ if (! empty($_GET['url'])) {
 
 // Load resources
 $resources = array(
-	'config.php',
-	'models/Database.php',
-	'models/DVD.php', 
-	'models/Video.php',
-	'models/Comment.php',
-	'models/Vote.php'
+	'../config.php',
+	'../models/Database.php',
+	'../models/DVD.php', 
+	'../models/Video.php',
+	'../models/Comment.php',
+	'../models/Vote.php'
 );
 
 foreach ($resources as $resource) {
@@ -23,23 +23,23 @@ foreach ($resources as $resource) {
 
 // Include the right controller
 if (! empty($URL) && $URL[0] == 'create') {
-	include('controllers/createDVD.php');
+	include('../controllers/createDVD.php');
 } elseif (! empty($URL) && $URL[0] == 'vote') {
-	include('controllers/vote.php');
+	include('../controllers/vote.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && empty($URL[2])) {
-	include('controllers/showDVD.php');
+	include('../controllers/showDVD.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && $URL[2] == 'suggest') {
-	include('controllers/suggestVideo.php');
+	include('../controllers/suggestVideo.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && $URL[2] == 'comment') {
-	include('controllers/addComment.php');
+	include('../controllers/addComment.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && ! empty($URL[2]) && empty($URL[3])) {
-	include('controllers/admin.php');
+	include('../controllers/admin.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && ! empty($URL[2]) && $URL[3] == 'settings') {
-	include('controllers/saveSettings.php');
+	include('../controllers/saveSettings.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && ! empty($URL[2]) && $URL[3] == 'deleteVideo') {
-	include('controllers/deleteVideo.php');
+	include('../controllers/deleteVideo.php');
 } elseif (! empty($URL) && (int) $URL[0] > 0 && ! empty($URL[1]) && ! empty($URL[2]) && $URL[3] == 'deleteDVD') {
-	include('controllers/deleteDVD.php');
+	include('../controllers/deleteDVD.php');
 } else {
-	include('controllers/viewFrontpage.php');
+	include('../controllers/viewFrontpage.php');
 }
