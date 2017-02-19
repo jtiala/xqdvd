de7693ff2ef66d322a674d733db8730fa2ba91b0
Joonas Tiala <joonas.tiala@gmail.com>
2017-02-19 14:42:58 +0200

    Added fork me ribbon

--- views/footer.php
+++ views/footer.php
@@ -5,5 +5,6 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 		<script src="<?= SITE_URL ?>/assets/js/scripts.js"></script>
+		<a class="github-fork-ribbon" href="https://github.com/jtiala/xqdvd" title="Fork me on GitHub">Fork me on GitHub</a>
 	</body>
 </html>
--- views/header.php
+++ views/header.php
@@ -9,9 +9,11 @@
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="<?= SITE_URL ?>/assets/css/style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.min.css" />
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.ie.min.css" />
 		<![endif]-->
 	</head>
 	<body>
